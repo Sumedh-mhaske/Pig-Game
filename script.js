255882eaa4560e0d692dfae329a595c424e8146b
Sumedh Mhaske <sumedhmhaske310503@gmail.com>
2024-06-07 14:26:15 +0530

    Player wins if score is >= 100, either switch player

--- script.js
+++ script.js
@@ -83,4 +83,21 @@ btnHold.addEventListener("click", function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
   }
+
+  // 2. Check if player's score is >= 100
+  if (scores[activePlayer] >= 100) {
+    // Finish the game
+    playing = false;
+    diceEl.classList.add("hidden");
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add("player--winner");
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove("player--active");
+  } else {
+    // Switch to the next player
+
+    switchPlayer();
+  }
 });

e473a4757bd0ea7a5bac2b4b35c3364f056b7421
Sumedh Mhaske <sumedhmhaske310503@gmail.com>
2024-06-07 14:16:06 +0530

    Check if rolled dice contains 1

--- script.js
+++ script.js
@@ -58,4 +58,17 @@ btnRoll.addEventListener("click", function () {
     diceEl.classList.remove("hidden");
     diceEl.src = `dice-${dice}.png`;
   }
+
+  // 3. Check for rolled 1
+
+  if (dice !== 1) {
+    // Add dice to the cureent score
+
+    currentScore = currentScore + dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    // Switch to next player
+    switchPlayer();
+  }
 });
